5e1f8cc4832d1d8eb47088ac0abd8f21926f9452
princebaloch1251-ui <princebaloch1251@gmail.com>
2026-01-21 00:43:05 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 ❖✧𝕱✦𝖆✧𝖓✦𝖎✧❖ 𓆪',
+  author: '‎',
+  botName: "𓆩 ❖✧𝕱✦𝖆✧𝖓✦𝖎✧❖ 𓆪",
+  botOwner: '❖✧𝕱✦𝖆✧𝖓✦𝖎✧❖', // Your name
+  ownerNumber: '923315404041', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/princebaloch1251-ui/-?tab=readme-ov-file#-",
+};
+
+module.exports = settings;
